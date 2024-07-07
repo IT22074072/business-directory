dbc11fad969de7bc2a465d5e8cb93b2dce37b7b7
IT22074072 <it22074072@my.sliit.lk>
2024-07-07 19:28:37 +0530

    Setup and first run

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        
+      }}
+    >
+      <Text>Hello Dinithii !</Text>
+    </View>
+  );
+}
