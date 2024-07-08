f803e91cb8c6722254d9cfe3e7fcae9134fcc3df
IT22074072 <it22074072@my.sliit.lk>
2024-07-08 14:04:51 +0530

    add img, text with internal style

--- components/LoginScreen.jsx
+++ components/LoginScreen.jsx
@@ -1,10 +1,34 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { Colors } from '@/constants/Colors';
 
 export default function LoginScreen() {
   return (
     <View>
-      <Text style={{fontSize:30}}>LoginScreen</Text>
+      <View style={{
+        display:'flex',
+        alignItems:'center',
+        marginTop:100
+      }}>
+      <Image source={require("./../assets/images/login.png")}
+          style={{
+            width:200,
+            height:400,
+            borderRadius:20,
+            borderWidth:6,
+            borderColor:'#000'
+
+
+          } 
+          }
+      />
+      </View>
+      
+      <View style={{backgroundColor:'#fff', padding:20}}>
+        <Text>Your Ultimate <Text style={{color:Colors.PRIMARY}}>Community Business Directory</Text> App</Text>
+      </View>
     </View>
-  )
+  );
 }
+
+
