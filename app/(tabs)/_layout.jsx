2e05a15268216925483d57cd13c10bc753f61204
IT22074072 <it22074072@my.sliit.lk>
2024-07-08 10:46:03 +0530

    Add icons to the tab bar

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,13 +1,20 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import {Tabs} from 'expo-router'
+import { FontAwesome } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
   return (
     <Tabs screenOptions={{headerShown:false}}>
-        <Tabs.Screen name = 'home'/>
-        <Tabs.Screen name = 'explore'/>
-        <Tabs.Screen name = 'profile'/>
+        <Tabs.Screen name = 'home'
+        options={{tabBarLabel:'Home', tabBarIcon:({color})=> <FontAwesome name="home" size={24} color={color} />}}/>
+
+        <Tabs.Screen name = 'explore'
+        options={{tabBarLabel:'Search', tabBarIcon:({color})=> <FontAwesome name="search" size={24} color={color} />}}/>
+
+        <Tabs.Screen name = 'profile'
+        options={{tabBarLabel:'Profile', tabBarIcon:({color})=><Ionicons name="person-circle" size={24} color={color}/>}}/>
     </Tabs>
   )
 }
