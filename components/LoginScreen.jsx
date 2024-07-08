975338d9da1f54fee59fa06e34d44ad6f6b7d201
IT22074072 <it22074072@my.sliit.lk>
2024-07-08 14:30:09 +0530

    Text(Another description) and external style

--- components/LoginScreen.jsx
+++ components/LoginScreen.jsx
@@ -1,34 +1,59 @@
 import { View, Text, Image } from "react-native";
 import React from "react";
-import { Colors } from '@/constants/Colors';
+import { Colors } from "@/constants/Colors";
+import { StyleSheet } from "react-native";
 
 export default function LoginScreen() {
   return (
     <View>
-      <View style={{
-        display:'flex',
-        alignItems:'center',
-        marginTop:100
-      }}>
-      <Image source={require("./../assets/images/login.png")}
+      <View
+        style={{
+          display: "flex",
+          alignItems: "center",
+          marginTop: 100,
+        }}
+      >
+        <Image
+          source={require("./../assets/images/login.png")}
           style={{
-            width:200,
-            height:400,
-            borderRadius:20,
-            borderWidth:6,
-            borderColor:'#000'
-
-
-          } 
-          }
-      />
+            width: 200,
+            height: 400,
+            borderRadius: 20,
+            borderWidth: 6,
+            borderColor: "#000",
+          }}
+        />
       </View>
-      
-      <View style={{backgroundColor:'#fff', padding:20}}>
-        <Text>Your Ultimate <Text style={{color:Colors.PRIMARY}}>Community Business Directory</Text> App</Text>
+
+      <View style={styles.subContainer}>
+        <Text
+          style={{
+            fontSize: 25,
+            fontFamily: "outfit-bold",
+            textAlign: "center",
+          }}
+        >
+          Your Ultimate
+          <Text
+            style={{
+              color: Colors.PRIMARY,
+            }}
+          > {""}
+            Community Business Directory
+          </Text> {""}
+          App
+        </Text>
+
+        <Text style={{fontSize:15, fontFamily:'outfit', textAlign:'center', marginVertical:15, color:Colors.GRAY}}>Find your favorite business near you and post your own business to your community</Text>
       </View>
     </View>
   );
 }
 
-
+const styles = StyleSheet.create({
+  subContainer: {
+    backgroundColor: "#fff",
+    padding: 20,
+    marginTop:-20
+  },
+});
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -24,6 +24,7 @@ export const Colors = {
     tabIconSelected: tintColorDark,
   },
 
-  PRIMARY:'#7F57F1'
+  PRIMARY:'#7F57F1',
+  GRAY:'#8F8F8F'
 
 };
