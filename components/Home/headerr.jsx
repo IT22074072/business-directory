768de65151b59ba4b65de6d1b888fd14cc344c4d
IT22074072 <it22074072@my.sliit.lk>
2024-07-08 20:14:51 +0530

    Add text to the Header component with user name

--- components/Home/headerr.jsx
+++ components/Home/headerr.jsx
@@ -1,26 +1,50 @@
-import { View, Image } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 import { useUser } from "@clerk/clerk-expo";
+import { Colors } from "../../constants/Colors";
+import { SearchBar } from "react-native-screens";
 
 export default function Headerr() {
-    const { user } = useUser();
-    return (
+  const { user } = useUser();
+  return (
+    <View
+      style={{
+        padding: 20,
+        paddingTop: 40,
+        backgroundColor: Colors.PRIMARY,
+      }}
+    >
       <View
         style={{
-          padding: 20,
-          paddingTop: 40,
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 10,
         }}
       >
+        <Image
+          source={{ uri: user?.imageUrl }}
+          style={{
+            width: 45,
+            height: 45,
+            borderRadius: 99,
+          }}
+        />
+
         <View>
-          <Image
-            source={{ uri: user?.imageUrl }}
+          <Text style={{ color: "#fff" }}>Welcome, </Text>
+          <Text
             style={{
-              width: 45,
-              height: 45,
-              borderRadius: 99,
+              fontSize: 19,
+              fontFamily: "outfit-medium",
+              color: "#fff",
             }}
-          />
+          >
+            {user?.fullName}
+          </Text>
         </View>
       </View>
-    );
+
+    </View>
+  );
 }
