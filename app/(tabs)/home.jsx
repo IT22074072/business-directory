105b29ba3dd9061fb854b6836d898645c120d2e6
IT22074072 <it22074072@my.sliit.lk>
2024-07-08 11:27:27 +0530

    Add customized font and use it

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function home() {
   return (
     <View>
-      <Text>home</Text>
+      <Text style={{fontSize:40,fontFamily:'outfit-bold'}}>home</Text>
     </View>
   )
 }
