8ccb1959cdab53e1207d42d1796249ffe46df40e
IT22074072 <it22074072@my.sliit.lk>
2024-07-18 09:33:15 +0530

    replace header by category in the selected category page

--- app/businesslist/[category].js
+++ app/businesslist/[category].js
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React, { useEffect } from "react";
+import { useLocalSearchParams, useNavigation } from "expo-router";
 
 export default function BusinessListByCategory() {
+  const navigation = useNavigation();
+  const { category } = useLocalSearchParams();
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      headerTitle: category,
+    });
+  }, []);
+
   return (
     <View>
-      <Text>BusinessListByCategory</Text>
+      <Text>{category}</Text>
     </View>
-  )
+  );
 }
