6f65c97c0d0445772f63b3c99ae619f8fa0b37f1
IT22074072 <it22074072@my.sliit.lk>
2024-07-19 13:43:02 +0530

    Add actions

--- components/BusinessDetail/ActionButton.jsx
+++ components/BusinessDetail/ActionButton.jsx
@@ -1,20 +1,38 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 
 export default function ActionButton(business) {
     const actionButtonMenu = [
         {
             id:1,
-            name:'call',
+            name:'Call',
             icon:'./../../assets/images/call.png',
-            url:
-        }
+            url:'tel'+business?.contact
+        },
+        {
+            id:2,
+            name:'Location',
+            icon:'./../../assets/images/pin.png',
+            url:'https://www.google.com/maps/search/?api=1&query='+business?.address
+        },
+        {
+            id:3,
+            name:'Web',
+            icon:'./../../assets/images/web.png',
+            url:'tel'+business?.website
+        },
+        {
+            id:4,
+            name:'Share',
+            icon:'./../../assets/images/share.png',
+            url:'tel'+business?.website
+        },
     ]
 
 
   return (
     <View style={{backgroundColor:'#fff', padding:20}}>
-      <Text>ActionButton</Text>
+      
     </View>
   )
 }
