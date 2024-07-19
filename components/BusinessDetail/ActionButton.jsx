2c05be14ba2cb8d2677e8632665d272429f5436b
IT22074072 <it22074072@my.sliit.lk>
2024-07-19 23:02:16 +0530

    share function

--- components/BusinessDetail/ActionButton.jsx
+++ components/BusinessDetail/ActionButton.jsx
@@ -1,55 +1,81 @@
-import { View, Text, FlatList, Image, TouchableOpacity, Linking } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  TouchableOpacity,
+  Linking,
+  Share,
+} from "react-native";
+import React from "react";
 
-export default function ActionButton(business) {
-    const actionButtonMenu = [
-        {
-            id:1,
-            name:'Call',
-            icon:require('./../../assets/images/call.png'),
-            url:'tel:'+business?.contact
-        },
-        {
-            id:2,
-            name:'Location',
-            icon:require('./../../assets/images/pin.png'),
-            url:'https://www.google.com/maps/search/?api=1&query='+business?.address
-        },
-        {
-            id:3,
-            name:'Web',
-            icon:require('./../../assets/images/web.png'),
-            url: business?.website?.startsWith('http') ? business.website : 'http://' + business.website
-        },
-        {
-            id:4,
-            name:'Share',
-            icon:require('./../../assets/images/share.png'),
-            url:business?.website
-        },
-    ]
+export default function ActionButton({business}) {
+  const actionButtonMenu = [
+    {
+      id: 1,
+      name: "Call",
+      icon: require("./../../assets/images/call.png"),
+      url: "tel:" + business?.contact,
+    },
+    {
+      id: 2,
+      name: "Location",
+      icon: require("./../../assets/images/pin.png"),
+      url:
+        "https://www.google.com/maps/search/?api=1&query=" + business?.address,
+    },
+    {
+      id: 3,
+      name: "Web",
+      icon: require("./../../assets/images/web.png"),
+      url: business?.website?.startsWith("http")
+        ? business.website
+        : "http://" + business.website,
+    },
+    {
+      id: 4,
+      name: "Share",
+      icon: require("./../../assets/images/share.png"),
+      url: null,
+    },
+  ];
 
-
-    const OnPressHandle=(item)=>{
-        if(item.name=='Share'){
-            return ;
-        }
-        Linking.openURL(item.url)
+  const OnPressHandle = (item) => {
+    if (item.name == "Share") {
+      Share.share({
+        message:
+          business?.name +
+          "\n Address:" +
+          business.address +
+          "\n Find more details on Kpop Business Directory App by Dinithi !",
+      });
+      return;
+    } else {
+      Linking.openURL(item.url);
     }
+  };
 
   return (
-    <View style={{backgroundColor:'#fff', padding:20}}>
+    <View style={{ backgroundColor: "#fff", padding: 20 }}>
       <FlatList
-      data={actionButtonMenu}
-      numColumns={4}
-      columnWrapperStyle={{justifyContent:'space-between'}}
-      renderItem={({item, index})=>(
-        <TouchableOpacity key={index} onPress={()=>OnPressHandle(item)}>
-            <Image source={item?.icon} style={{width:40, height:40}}/>
-            <Text style={{fontFamily:'outfit-medium', textAlign:'center', marginTop:3}}>{item.name}</Text>
-        </TouchableOpacity>
-      )}
+        data={actionButtonMenu}
+        numColumns={4}
+        columnWrapperStyle={{ justifyContent: "space-between" }}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity key={index} onPress={() => OnPressHandle(item)}>
+            <Image source={item?.icon} style={{ width: 40, height: 40 }} />
+            <Text
+              style={{
+                fontFamily: "outfit-medium",
+                textAlign: "center",
+                marginTop: 3,
+              }}
+            >
+              {item.name}
+            </Text>
+          </TouchableOpacity>
+        )}
       />
     </View>
-  )
+  );
 }
