d202a2995fc33f7bfd18055a88086ed45772dd96
IT22074072 <it22074072@my.sliit.lk>
2024-07-19 15:24:55 +0530

    Complete action buttons exept share

--- components/BusinessDetail/ActionButton.jsx
+++ components/BusinessDetail/ActionButton.jsx
@@ -1,4 +1,4 @@
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, Image, TouchableOpacity, Linking } from 'react-native'
 import React from 'react'
 
 export default function ActionButton(business) {
@@ -6,33 +6,50 @@ export default function ActionButton(business) {
         {
             id:1,
             name:'Call',
-            icon:'./../../assets/images/call.png',
-            url:'tel'+business?.contact
+            icon:require('./../../assets/images/call.png'),
+            url:'tel:'+business?.contact
         },
         {
             id:2,
             name:'Location',
-            icon:'./../../assets/images/pin.png',
+            icon:require('./../../assets/images/pin.png'),
             url:'https://www.google.com/maps/search/?api=1&query='+business?.address
         },
         {
             id:3,
             name:'Web',
-            icon:'./../../assets/images/web.png',
-            url:'tel'+business?.website
+            icon:require('./../../assets/images/web.png'),
+            url: business?.website?.startsWith('http') ? business.website : 'http://' + business.website
         },
         {
             id:4,
             name:'Share',
-            icon:'./../../assets/images/share.png',
-            url:'tel'+business?.website
+            icon:require('./../../assets/images/share.png'),
+            url:business?.website
         },
     ]
 
 
+    const OnPressHandle=(item)=>{
+        if(item.name=='Share'){
+            return ;
+        }
+        Linking.openURL(item.url)
+    }
+
   return (
     <View style={{backgroundColor:'#fff', padding:20}}>
-      
+      <FlatList
+      data={actionButtonMenu}
+      numColumns={4}
+      columnWrapperStyle={{justifyContent:'space-between'}}
+      renderItem={({item, index})=>(
+        <TouchableOpacity key={index} onPress={()=>OnPressHandle(item)}>
+            <Image source={item?.icon} style={{width:40, height:40}}/>
+            <Text style={{fontFamily:'outfit-medium', textAlign:'center', marginTop:3}}>{item.name}</Text>
+        </TouchableOpacity>
+      )}
+      />
     </View>
   )
 }
