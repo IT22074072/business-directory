d579b8d6705f329284e35394a8ad16bf87589ae1
IT22074072 <it22074072@my.sliit.lk>
2024-07-19 10:29:41 +0530

    Finish GetBusinessDetailById  function

--- app/businessdetail/[businessid].jsx
+++ app/businessdetail/[businessid].jsx
@@ -1,34 +1,42 @@
-import { View, Text } from 'react-native'
-import React, { useEffect } from 'react'
-import { useLocalSearchParams } from 'expo-router'
-import {db} from './../../configs/FirebaseConfig'
-import { doc, getDoc } from 'firebase/firestore'
+import { View, Text, ActivityIndicator } from "react-native";
+import React, { useEffect, useState } from "react";
+import { useLocalSearchParams } from "expo-router";
+import { db } from "./../../configs/FirebaseConfig";
+import { doc, getDoc } from "firebase/firestore";
+import { Colors } from './../../constants/Colors'
 
 export default function businessDetail() {
-    const {businessid} = useLocalSearchParams();
+  const { businessid } = useLocalSearchParams();
+  const [businessDetail, setBusinessDetail] = useState();
+  const [loading, setLoading] = useState(false);
 
-    useEffect(()=>{
-      GetBusinessDetailById();
-    }, [])
-    /**
-     * Used to get BusinessDetail by Id
-     */
-    const GetBusinessDetailById=async()=>{
-
-      const docRef = doc(db, 'BusinessList', businessid);
-      const docSnap=await getDoc(docRef);
-      if(docSnap.exists()){
-        console.log("Document data:", docSnap.data());
-      }
-      else{
-        //docSnap.data() will be undefined
-        console.log("No such document!");
-      }
-
+  useEffect(() => {
+    GetBusinessDetailById();
+  }, []);
+  /**
+   * Used to get BusinessDetail by Id
+   */
+  const GetBusinessDetailById = async () => {
+    setLoading(true);
+    const docRef = doc(db, "BusinessList", businessid);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      setBusinessDetail(docSnap.data());
+      setLoading(false);
+    } else {
+      //docSnap.data() will be undefined
+      console.log("No such document!");
     }
+  };
   return (
     <View>
-      <Text>{businessid}</Text>
+      {loading ? (
+        <ActivityIndicator style={{marginTop:'60%'}} size={"large"} color={Colors.PRIMARY}/>
+      ) : (
+        <View>
+          
+        </View>
+      )}
     </View>
-  )
+  );
 }
