a3b4b499b8762ae25ea03d887245a04055b545b3
IT22074072 <it22074072@my.sliit.lk>
2024-07-21 11:31:40 +0530

    App signout and share

--- components/Profile/MenuList.jsx
+++ components/Profile/MenuList.jsx
@@ -1,56 +1,103 @@
-import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { Colors } from '../../constants/Colors'
-import { useRouter } from 'expo-router'
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  TouchableOpacity,
+  Share,
+} from "react-native";
+import React from "react";
+import { Colors } from "../../constants/Colors";
+import { useRouter } from "expo-router";
+import { SignedOut } from "@clerk/clerk-expo";
 
 export default function MenuList() {
-    const router =useRouter();
+  const router = useRouter();
 
-    const onMenuClick=(item)=>{
-
-        router.push(item.path)
-
+  const onMenuClick = (item) => {
+    if (item.path == "logout") {
+      SignedOut();
+      return;
     }
-    const menuList=[{
-        id:1,
-        name:'Add Business',
-        icon:require('./../../assets/images/add.png'),
-        path:'/business/add-business'
-    },
-    {
-        id:2,
-        name:'My Business',
-        icon:require('./../../assets/images/business-and-trade.png'),
-        path:'/business/my-business'
-    },
-    {
-        id:3,
-        name:'Share App',
-        icon:require('./../../assets/images/share_1.png'),
-        path:''
-    },
-    {
-        id:4,
-        name:'Logout',
-        icon:require('./../../assets/images/logout.png'),
-        path:''
+
+    if (item.path == "share") {
+      Share.share({
+        message:
+          "Download the kpop merchandise Business Directory App by Dinithi, Download URL:",
+      });
+
+      return;
     }
-]
+    router.push(item.path);
+  };
+  const menuList = [
+    {
+      id: 1,
+      name: "Add Business",
+      icon: require("./../../assets/images/add.png"),
+      path: "/business/add-business",
+    },
+    {
+      id: 2,
+      name: "My Business",
+      icon: require("./../../assets/images/business-and-trade.png"),
+      path: "/business/my-business",
+    },
+    {
+      id: 3,
+      name: "Share App",
+      icon: require("./../../assets/images/share_1.png"),
+      path: "share",
+    },
+    {
+      id: 4,
+      name: "Logout",
+      icon: require("./../../assets/images/logout.png"),
+      path: "logout",
+    },
+  ];
 
   return (
-    <View style={{marginTop:35}}>
+    <View style={{ marginTop: 35 }}>
       <FlatList
-      data={menuList}
-      numColumns={2}
-      renderItem={({item, index})=>(
-        <TouchableOpacity onPress={()=>onMenuClick(item)} style={{display:'flex', flexDirection:'row',alignItems:'center', gap:5, flex:1, borderRadius:10, borderWidth:1, margin:10, backgroundColor:'#fff', borderColor:Colors.PRIMARY}}>
-            <Image source={item.icon} style={{width:50, height:50}}/>
-            <Text style={{fontFamily:'outfit-medium', fontSize:15, flex:1}}>{item.name}</Text>
-        </TouchableOpacity>
-      )}
+        data={menuList}
+        numColumns={2}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity
+            onPress={() => onMenuClick(item)}
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 5,
+              flex: 1,
+              borderRadius: 10,
+              borderWidth: 1,
+              margin: 10,
+              backgroundColor: "#fff",
+              borderColor: Colors.PRIMARY,
+            }}
+          >
+            <Image source={item.icon} style={{ width: 50, height: 50 }} />
+            <Text
+              style={{ fontFamily: "outfit-medium", fontSize: 15, flex: 1 }}
+            >
+              {item.name}
+            </Text>
+          </TouchableOpacity>
+        )}
       />
 
-      <Text style={{fontFamily:'outfit', textAlign:'center', marginTop:45, color:Colors.GRAY}}>Developed by Dinithi @ 2024</Text>
+      <Text
+        style={{
+          fontFamily: "outfit",
+          textAlign: "center",
+          marginTop: 45,
+          color: Colors.GRAY,
+        }}
+      >
+        Developed by Dinithi @ 2024
+      </Text>
     </View>
-  )
+  );
 }
